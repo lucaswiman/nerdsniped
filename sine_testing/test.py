a72b3981c4357c74ae846663125f16cd5171ac3c
Lucas Wiman <lucaswiman@counsyl.com>
2018-12-17 12:09:58 -0800

    Add test that reference implementation works correctly.

--- sine_testing/test.py
+++ sine_testing/test.py
@@ -1,5 +1,18 @@
+import math
+
+import pytest
+
 import trig
+
+
+π = math.pi
 
 
 def test_sine_of_zero_is_zero():
     assert trig.sin(0) == 0
+
+def test_known_values():
+    assert trig.sin(π) == pytest.approx(0)
+    assert trig.sin(π / 2) == pytest.approx(1)
+    assert trig.sin(π / 4) == pytest.approx(math.sqrt(1 / 2))
+    assert trig.sin(2 * π / 3) == pytest.approx(math.sqrt(3) / 2)
--- sine_testing/trig.py
+++ sine_testing/trig.py
@@ -1,2 +1,8 @@
+import os
+
+
 def sin(x):
     return x
+
+if os.environ.get('USE_REFERENCE_IMPLEMENTATION'):
+    from math import sin
